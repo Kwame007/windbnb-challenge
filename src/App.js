b2a3f202bf61928407fed4e1423481b0fbf3f2c1
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-17 15:40:59 +0000

    display modal in dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Modal, { BackDrop } from "./components/Modal";
 import Navigation from "./components/Navigation";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="container mx-auto mt-5">
         <Navigation />
+        <Modal />
       </div>
     </>
   );
