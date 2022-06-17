aabb00ecf6466ab45988057fa29fdbe83833ffd4
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-17 16:16:15 +0000

    log context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
+import { useContext } from "react";
+import ModalContext from "./context/Context";
 
 function App() {
+  const test = useContext(ModalContext);
+  console.log(test);
   return (
     <>
       <div className="container mx-auto mt-5">
