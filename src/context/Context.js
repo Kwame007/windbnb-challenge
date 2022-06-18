06b8d3c1eb6f155d7b103e1976853486d13afd8c
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-18 01:23:48 +0000

    add new app wide states & manage states with useReducer

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,15 +1,87 @@
-import { createContext } from "react";
+import { useReducer, createContext } from "react";
 
 const ModalContext = createContext({
   isShowing: false,
+  locationSelector: false,
+  guestSelector: false,
+  showLocationSelector: () => {},
+  hideLocationSelector: () => {},
+  showGuestSelector: () => {},
+  hideGuestSelector: () => {},
   showModal: () => {},
   hideModal: () => {},
 });
 
+// reducer function
+const reducer = (state, action) => {
+  console.log(state);
+  switch (action.type) {
+    case "SHOW_MODAL":
+      return { ...state, isShowing: true };
+    case "HIDE_MODAL":
+      return { ...state, isShowing: false };
+    case "SHOW_LOCATION_SELECTOR":
+      return { ...state, locationSelector: true };
+    case "SHOW_GUEST_SELECTOR":
+      return { ...state, guestSelector: true };
+    case "HIDE_LOCATION_SELECTOR":
+      return { ...state, locationSelector: false };
+    case "HIDE_GUEST_SELECTOR":
+      return { ...state, guestSelector: false };
+    default:
+      return state;
+  }
+};
+
 export const ModalProvider = (props) => {
+  // manage state with useReducer
+  const [state, dispatch] = useReducer(reducer, {
+    isShowing: false,
+    locationSelector: false,
+    guestSelector: false,
+  });
+
+  // handleShowModal
+  const handleShowModal = () => {
+    dispatch({ type: "SHOW_MODAL" });
+  };
+  // handleHideModal
+  const handleHideModal = () => {
+    dispatch({ type: "HIDE_MODAL" });
+  };
+
+  // handleShowLocationSelector
+  const handleShowLocationSelector = () => {
+    dispatch({ type: "SHOW_LOCATION_SELECTOR" });
+  };
+
+  // handleShowGuestSelector
+  const handleShowGuestSelector = () => {
+    dispatch({ type: "SHOW_GUEST_SELECTOR" });
+  };
+
+  // handleHideLocationSelector
+  const handleHideLocationSelector = () => {
+    dispatch({ type: "HIDE_LOCATION_SELECTOR" });
+  };
+  // handleHideGuestSelector
+  const handleHideGuestSelector = () => {
+    dispatch({ type: "HIDE_GUEST_SELECTOR" });
+  };
+
   return (
     <ModalContext.Provider
-      value={{ isShowing: false, showModal: () => {}, hideModal: () => {} }}
+      value={{
+        isShowing: state.isShowing,
+        locationSelector: state.locationSelector,
+        guestSelector: state.guestSelector,
+        showModal: handleShowModal,
+        hideModal: handleHideModal,
+        showLocationSelector: handleShowLocationSelector,
+        showGuestSelector: handleShowGuestSelector,
+        hideLocationSelector: handleHideLocationSelector,
+        hideGuestSelector: handleHideGuestSelector,
+      }}
     >
       {props.children}
     </ModalContext.Provider>
