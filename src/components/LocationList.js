29d179b34207e6dd7e8e23272303189582b969d3
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-18 01:18:50 +0000

    add sample locations array

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -4,8 +4,8 @@ import { IoMdPin } from "react-icons/io";
 const LocationList = (props) => {
   return (
     <ul className="ml-10">
-      {props?.locations?.map((list) => (
-        <div className="flex">
+      {props?.locations?.map((list, index) => (
+        <div className="flex" key={index}>
           <IoMdPin className="mr-1 inline" />
           <li className="mb-4 text tracking-wider text-sm text-gray-400 md:hover:cursor-pointer">
             {list}
