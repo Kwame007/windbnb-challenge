b20386d99af86d6d9f322eaa9b2c41684c6de385
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-18 01:22:38 +0000

    use context in modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import ModalContext from "../context/Context";
 import { IoIosSearch, IoMdClose } from "react-icons/io";
 import Button from "./Button";
 import GuestSelector from "./GuestSelector";
@@ -12,17 +13,50 @@ const locations = [
   "Vaasa, Finland",
 ];
 
-const BackDrop = () => {
-  return <div className="fixed inset-0 z-40 bg-black opacity-30" />;
+// backdrop component
+const BackDrop = (props) => {
+  return (
+    <div
+      className="fixed inset-0 z-40 bg-black opacity-30"
+      onClick={props.hideModal}
+    />
+  );
 };
 
 const Modal = () => {
+  // reducer
+  const {
+    hideModal,
+    locationSelector,
+    guestSelector,
+    showGuestSelector,
+    showLocationSelector,
+    hideGuestSelector,
+    hideLocationSelector,
+  } = useContext(ModalContext);
+
+  //show location selector on location input focus
+  const locationInputFocus = () => {
+    showLocationSelector();
+
+    // hide guest selector if location selector is shown
+    hideGuestSelector();
+  };
+
+  // show guest selector on guest input focus
+  const guestInputFocus = () => {
+    showGuestSelector();
+
+    // hide location selector if guest selector is shown
+    hideLocationSelector();
+  };
+
   return (
     <>
       <div className="p-3 bg-white fixed inset-0 max-h-fit z-50">
         <div className="grid grid-cols-2 md:hidden">
           <small className="block text-xs">Edit your search</small>
-          <Button className="text-xl w-fit ml-40">
+          <Button className="text-xl w-fit ml-40" onClick={hideModal}>
             <IoMdClose />
           </Button>
         </div>
@@ -38,6 +72,7 @@ const Modal = () => {
               <Input
                 className="border-0 py-4 px-2 w-full rounded-t-2xl placeholder:text-gray-500 placeholder:text-sm focus:outline-none"
                 placeholder="Helsinki,Finland"
+                onFocus={locationInputFocus}
               />
             </div>
             <div className=" md:border-r">
@@ -47,6 +82,7 @@ const Modal = () => {
               <Input
                 className="border-0 py-4 px-2 w-full rounded-b-2xl   placeholder:text-gray-500 placeholder:text-sm focus:outline-none"
                 placeholder="Add guest"
+                onFocus={guestInputFocus}
               />
             </div>
             <Button className=" hidden w-32 h-12  justify-center items-center bg-red-400 text-white text-sm rounded-2xl mx-auto md:my-auto md:flex ">
@@ -55,15 +91,16 @@ const Modal = () => {
           </div>
         </aside>
         <div className="grid grid-cols-1 mt-5 md:grid-cols-3 md:mx-auto md:mt-10 md:w-9/12">
-          <LocationList locations={locations} />
-          {/* <GuestSelector /> */}
+          {locationSelector && <LocationList locations={locations} />}
+          {guestSelector && <GuestSelector />}
         </div>
 
         <Button className=" w-32 h-12 flex justify-center items-center bg-red-400 text-white text-sm rounded-2xl mx-auto mt-10 md:hidden">
           <IoIosSearch className="mr-2 text-sm" /> Search
         </Button>
       </div>
-      <BackDrop />
+      {/* set modal backrop */}
+      <BackDrop hideModal={hideModal} />
     </>
   );
 };
