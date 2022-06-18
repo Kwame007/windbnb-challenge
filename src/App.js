f6b4905a4ec016cd0b0dd65b84e094c228ad821f
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-18 01:14:56 +0000

    use isShowing context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import { createPortal } from "react-dom";
 import Modal from "./components/Modal";
 import Navigation from "./components/Navigation";
 import { useContext } from "react";
 import ModalContext from "./context/Context";
 
 function App() {
-  const test = useContext(ModalContext);
-  console.log(test);
+  const { isShowing } = useContext(ModalContext);
+
   return (
     <>
       <div className="container mx-auto mt-5">
         <Navigation />
-        {ctx.isShowing && <Modal />}
+        {isShowing &&
+          createPortal(<Modal />, document.getElementById("modal-root"))}
       </div>
     </>
   );
