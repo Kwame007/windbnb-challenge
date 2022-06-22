66b29a42166993e7f0f1385bc22eb1ea7f0e454d
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-22 16:46:02 +0000

    fetch all stays data on component mount

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,21 +1,22 @@
-import { useReducer, createContext } from "react";
+import { useReducer, createContext, useEffect, useState } from "react";
 
 const ModalContext = createContext({
   isShowing: false,
   locationSelector: false,
   guestSelector: false,
-  adults: 1,
-  children: 2,
-  showLocationSelector: () => {},
-  hideLocationSelector: () => {},
-  showGuestSelector: () => {},
-  hideGuestSelector: () => {},
-  showModal: () => {},
-  hideModal: () => {},
-  increaseAdults: () => {},
-  decreaseAdults: () => {},
-  increaseChildren: () => {},
-  decreaseChildren: () => {},
+  // adults: 1,
+  // children: 2,
+  // filteredStays: [],
+  // showLocationSelector: () => {},
+  // hideLocationSelector: () => {},
+  // showGuestSelector: () => {},
+  // hideGuestSelector: () => {},
+  // showModal: () => {},
+  // hideModal: () => {},
+  // increaseAdults: () => {},
+  // decreaseAdults: () => {},
+  // increaseChildren: () => {},
+  // decreaseChildren: () => {},
 });
 
 // reducer function
@@ -34,23 +35,40 @@ const reducer = (state, action) => {
     case "HIDE_GUEST_SELECTOR":
       return { ...state, guestSelector: false };
     case "ADULT_INCREMENT":
-      console.log(state.adults);
-      return { ...state, adults: state.adults + 1 };
+      return {
+        ...state,
+        adults: state.adults + 1,
+        totalGuests: state.totalGuests + 1,
+      };
     case "ADULT_DECREMENT":
-      console.log(state.adults);
-      return { ...state, adults: state.adults - 1 };
+      return {
+        ...state,
+        adults: state.adults - 1,
+        totalGuests: state.totalGuests - 1,
+      };
     case "CHILD_INCREMENT":
-      console.log(state.children);
-      return { ...state, children: state.children + 1 };
+      return {
+        ...state,
+        children: state.children + 1,
+        totalGuests: state.totalGuests + 1,
+      };
     case "CHILD_DECREMENT":
-      console.log(state.children);
-      return { ...state, children: state.children - 1 };
+      return {
+        ...state,
+        children: state.children - 1,
+        totalGuests: state.totalGuests - 1,
+      };
+    case "FILTER_STAYS":
+      return { ...state, filteredStays: action.payload };
+    case "FETCH_STAYS":
+      return { ...state, stays: action.payload };
     default:
       return state;
   }
 };
 
 export const ModalProvider = (props) => {
+  const [staysData, setStaysData] = useState([]);
   // manage state with useReducer
   const [state, dispatch] = useReducer(reducer, {
     isShowing: false,
@@ -58,7 +76,34 @@ export const ModalProvider = (props) => {
     guestSelector: false,
     adults: 0,
     children: 0,
+    totalGuests: 0,
+    stays: [],
+    filteredStays: [],
   });
+
+  useEffect(() => {
+    // get data from json server
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3003/stays");
+
+        // check if res ok
+        if (!response.ok) {
+          throw new Error("Could not fetch data");
+        }
+
+        const data = await response.json();
+        setStaysData(data);
+        dispatch({ type: "FETCH_STAYS", payload: data });
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    return () => {
+      fetchData();
+    };
+  }, [staysData.length]);
 
   // handleShowModal
   const handleShowModal = () => {
@@ -108,6 +153,22 @@ export const ModalProvider = (props) => {
     dispatch({ type: "CHILD_DECREMENT" });
   };
 
+  // filter stays by location and guests
+  const filterStays = (stays, query, totalGuests) => {
+    // get value from location input
+    const queryString = query.split(",")[0].toLowerCase();
+
+    // filtered stays
+    const filteredStays = stays.filter((stay) => {
+      return (
+        stay.city.toLowerCase().includes(queryString) &&
+        totalGuests <= stay.maxGuests
+      );
+    });
+    dispatch({ type: "FILTER_STAYS", payload: filteredStays });
+    console.log(filteredStays);
+  };
+
   return (
     <ModalContext.Provider
       value={{
@@ -116,6 +177,9 @@ export const ModalProvider = (props) => {
         guestSelector: state.guestSelector,
         adults: state.adults,
         children: state.children,
+        totalGuests: state.totalGuests,
+        stays: state.stays,
+        filteredStays: state.filteredStays,
         showModal: handleShowModal,
         hideModal: handleHideModal,
         showLocationSelector: handleShowLocationSelector,
@@ -126,6 +190,7 @@ export const ModalProvider = (props) => {
         decreaseAdults: handleAdultDecrement,
         increaseChildren: handleChildIncrement,
         decreaseChildren: handleChildDecrement,
+        filterStays,
       }}
     >
       {props.children}
