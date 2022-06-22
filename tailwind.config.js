d4aedbefcc1bbbe87f6d48ef3147e014eaa9bcd4
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-22 10:30:38 +0000

    add google fonts to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      monserat: ["Montserrat", "sans-serif"],
+      muly: ["Mulish", " sans-serif"],
+    },
   },
   plugins: [],
 };
