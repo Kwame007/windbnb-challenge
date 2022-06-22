e5b8697a0c39380eda82d4c08ae44be25f085b64
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-22 16:37:41 +0000

    add dynamic rendering for filtered stays

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,41 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import Listing from "./Listing";
+import ModalContext from "../context/Context";
 
 const Listings = () => {
-  const [staysData, setStaysData] = useState([]);
+  const { stays, filteredStays } = useContext(ModalContext);
 
-  useEffect(() => {
-    // get data from json server
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3003/stays");
-
-        // check if res ok
-        if (!response.ok) {
-          throw new Error("Could not fetch data");
-        }
-
-        const data = await response.json();
-        setStaysData(data);
-        console.log(data);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-
-    return () => {
-      fetchData();
-    };
-  }, []);
   return (
     <section className="my-10 px-3 md:mt-40 ">
       <h1 className="text-lg font-bold font-monserat leading-6 text-gray-900 md:text-2xl">
         Stays in Finland
       </h1>
       <div className="grid grid-cols-1 gap-5 mt-5 md:grid-cols-4 md:w-fit md:gap-10 ">
-        {staysData?.map((data, index) => (
-          <Listing {...data} key={index} />
-        ))}
+        {/* render stays array if filter array===0 */}
+        {filteredStays.length === 0 &&
+          stays?.map((data, index) => <Listing {...data} key={index} />)}
+
+        {/* render filtered array if not ===0 */}
+        {filteredStays.length !== 0 &&
+          filteredStays?.map((data, index) => (
+            <Listing {...data} key={index} />
+          ))}
       </div>
     </section>
   );
