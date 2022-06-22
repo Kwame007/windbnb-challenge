18f40aca50fa6fa2d5ae4c303e75758f67174ba7
Kwame007 <clementnyankuadjei@gmail.com>
2022-06-22 10:14:39 +0000

    guest selector component

--- src/components/GuestSelector.js
+++ src/components/GuestSelector.js
@@ -1,16 +1,65 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button from "./Button";
+import ModalContext from "../context/Context";
 
-const GuestPicker = (props) => {
+const AdultPicker = ({
+  title,
+  desc,
+  adults,
+  increaseAdults,
+  decreaseAdults,
+}) => {
   return (
     <div className="ml-10">
       <div className="mb-5">
-        <p className="text-sm">{props.title}</p>
-        <p className="text-xs text-gray-400">{props.desc}</p>
+        <p className="text-sm">{title}</p>
+        <p className="text-xs text-gray-400">{desc}</p>
         <div className="flex mt-2">
-          <Button className="w-6 h-6 border rounded-md text-gray-400">-</Button>
-          <p className="px-5">0</p>
-          <Button className="w-6 h-6 border rounded-md text-gray-400">+</Button>
+          <Button
+            className="w-6 h-6 border rounded-md text-gray-400"
+            onClick={decreaseAdults}
+          >
+            -
+          </Button>
+          <p className="px-5">{adults}</p>
+          <Button
+            className="w-6 h-6 border rounded-md text-gray-400"
+            onClick={increaseAdults}
+          >
+            +
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ChildrenPicker = ({
+  title,
+  desc,
+  children,
+  increaseChildren,
+  decreaseChildren,
+}) => {
+  return (
+    <div className="ml-10">
+      <div className="mb-5">
+        <p className="text-sm">{title}</p>
+        <p className="text-xs text-gray-400">{desc}</p>
+        <div className="flex mt-2">
+          <Button
+            className="w-6 h-6 border rounded-md text-gray-400"
+            onClick={decreaseChildren}
+          >
+            -
+          </Button>
+          <p className="px-5">{children}</p>
+          <Button
+            className="w-6 h-6 border rounded-md text-gray-400"
+            onClick={increaseChildren}
+          >
+            +
+          </Button>
         </div>
       </div>
     </div>
@@ -18,11 +67,31 @@ const GuestPicker = (props) => {
 };
 
 const GuestSelector = () => {
+  const {
+    adults,
+    children,
+    increaseAdults,
+    decreaseAdults,
+    increaseChildren,
+    decreaseChildren,
+  } = useContext(ModalContext);
   return (
     <>
       <div className="md:col-start-2">
-        <GuestPicker title="Adult" desc="Age 13 or above" />
-        <GuestPicker title="Children" desc="Age 2 - 12" />
+        <AdultPicker
+          title="Adult"
+          desc="Age 13 or above"
+          adults={adults}
+          increaseAdults={increaseAdults}
+          decreaseAdults={decreaseAdults}
+        />
+        <ChildrenPicker
+          title="Children"
+          desc="Age 2 - 12"
+          children={children}
+          increaseChildren={increaseChildren}
+          decreaseChildren={decreaseChildren}
+        />
       </div>
     </>
   );
